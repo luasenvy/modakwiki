daec1535967fd8e561c38f212c5daa1b3a6afd46
luasenvy <luas.envy@gmail.com>
2025-09-05 17:14:15 +0900

    fix(page): signup page not use yet

--- app/[lng]/(auth)/signup/page.tsx
+++ app/[lng]/(auth)/signup/page.tsx
@@ -1,10 +1,9 @@
 import { GalleryVerticalEnd } from "lucide-react";
-import { CookiesProvider } from "react-cookie";
-import { SignupForm } from "@/components/pages/signup/SignupForm";
-import { Language } from "@/lib/i18n/config";
+// import { SignupForm } from "@/components/pages/signup/SignupForm";
+// import { Language } from "@/lib/i18n/config";
 
 export default async function SigninPage(ctx: PageProps<"/[lng]/signin">) {
-  const { lng: lngParam } = await ctx.params;
+  // const { lng: lngParam } = await ctx.params;
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
@@ -15,7 +14,7 @@ export default async function SigninPage(ctx: PageProps<"/[lng]/signin">) {
           Acme Inc.
         </a>
 
-        <SignupForm lng={lngParam as Language} />
+        {/* <SignupForm lng={lngParam as Language} /> */}
       </div>
     </div>
   );
