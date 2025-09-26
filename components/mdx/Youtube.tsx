1f7f8185eed145baad870a145a9531128d7deac1
luasenvy <luas.envy@gmail.com>
2025-09-26 11:58:16 +0900

    feat: enhance Youtube component

--- components/mdx/Youtube.tsx
+++ components/mdx/Youtube.tsx
@@ -6,10 +6,10 @@ export function Youtube({ v }: YoutubeProps) {
   const videos = v.split(",").map((vid) => vid.substring(2, vid.length - 1).trim());
 
   return (
-    <div className="my-4 flex items-center gap-4 max-xl:flex-col">
-      {videos.map((videoId) => (
+    <div className="my-4 flex items-center gap-4 overflow-x-auto max-xl:flex-col">
+      {videos.map((videoId, i) => (
         <iframe
-          key={`video-${videoId}`}
+          key={`video-${videoId}-${i}`}
           width="100%"
           height="400"
           src={`https://www.youtube.com/embed/${videoId}`}
