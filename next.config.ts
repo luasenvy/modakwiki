9159cd2f712850fd1881c06ab4bc53d8ef866a7e
luasenvy <luas.envy@gmail.com>
2025-10-04 15:19:09 +0900

    fix: remove poweredByHeader setting from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 export default {
   output: "standalone",
+  poweredByHeader: false,
   allowedDevOrigins: ["modak.wiki"],
   experimental: { authInterrupts: true },
   serverExternalPackages: ["knex"],
