b7103ebb84f32d46eb9065734603593172515a11
luasenvy <luas.envy@gmail.com>
2025-10-10 22:19:15 +0900

    fix: enhance sitemap caching and locale handling in feed route

--- app/api/feed/[type]/route.ts
+++ app/api/feed/[type]/route.ts
@@ -1,17 +1,43 @@
+import { MetadataRoute } from "next";
 import type { NextRequest } from "next/server";
 import { site } from "@/config";
 import { knex } from "@/lib/db";
 import { FeedType, feedColumns, feedEnum, frequencyEnum, getFeed } from "@/lib/feed";
-import { cookieName, languageEnum, languages } from "@/lib/i18n/config";
+import { cookieName, languages } from "@/lib/i18n/config";
 import { negotiate } from "@/lib/i18n/detect";
-import { Document as DocumentType, doctypeEnum } from "@/lib/schema/document";
+import { redis } from "@/lib/redis";
+import { doctypeEnum } from "@/lib/schema/document";
+import { HOUR } from "@/lib/time";
 import Logo from "@/public/brand/logo.webp";
+
+let expired = 0;
+
+function alternates(url: string) {
+  return {
+    languages: languages.reduce(
+      (acc, language) => ({
+        ...acc,
+        [language]: `${site.baseurl}/${language}${url}`,
+      }),
+      {},
+    ),
+  };
+}
 
 export async function GET(req: NextRequest, ctx: RouteContext<"/api/feed/[type]">) {
   const feed = await getFeed();
 
   const type = (await ctx.params).type as FeedType;
   if (feedEnum.sitemap === type) {
+    const locale = negotiate(
+      req.cookies.get(cookieName)?.value || req.headers.get("accept-language"),
+    );
+
+    if (!redis.isOpen) await redis.connect();
+
+    if (Date.now() <= expired)
+      return Response.json(await redis.json.get(`sitemap:${locale}`, { path: "." }));
+
     const rows = await knex
       .select({
         id: "o.id",
@@ -43,61 +69,74 @@ export async function GET(req: NextRequest, ctx: RouteContext<"/api/feed/[type]"
       .orderBy("created", "asc");
 
     const started = new Date();
-
-    const locale = negotiate(
-      req.cookies.get(cookieName)?.value || req.headers.get("accept-language"),
-    );
-
-    return Response.json([
+    const items = [
       {
         url: site.baseurl,
+        alternates: alternates(""),
         lastModified: new Date(),
         changeFrequency: frequencyEnum.monthly,
         priority: 1,
         images: [Logo.src],
       },
       {
-        url: `${site.baseurl}/${locale}/what-is-this`,
+        url: `${site.baseurl}/what-is-this`,
         changeFrequency: frequencyEnum.monthly,
         priority: 0.6,
         lastModified: started,
+        alternates: alternates("/what-is-this"),
       },
       {
-        url: `${site.baseurl}/${locale}/random`,
+        url: `${site.baseurl}/random`,
         changeFrequency: frequencyEnum.monthly,
         priority: 0.6,
         lastModified: started,
+        alternates: alternates("/random"),
       },
       {
-        url: `${site.baseurl}/${locale}/popular`,
+        url: `${site.baseurl}/popular`,
         changeFrequency: frequencyEnum.weekly,
         priority: 0.6,
         lastModified: started,
+        alternates: alternates("/popular"),
       },
       {
-        url: `${site.baseurl}/${locale}/list`,
+        url: `${site.baseurl}/list`,
         changeFrequency: frequencyEnum.weekly,
         priority: 0.6,
         lastModified: started,
+        alternates: alternates("/list"),
       },
       {
-        url: `${site.baseurl}/${locale}/series`,
+        url: `${site.baseurl}/series`,
         changeFrequency: frequencyEnum.weekly,
         priority: 0.6,
         lastModified: started,
+        alternates: alternates("/series"),
       },
       ...rows.map(({ id, type, images, created, updated }) => {
+        const url = `/${type}?${new URLSearchParams({ id })}`;
         return {
-          url: `${site.baseurl}/${locale}/${type}?${new URLSearchParams({ id })}`,
+          url: `${site.baseurl}${url}`,
           changeFrequency: frequencyEnum.monthly,
           priority: 0.6,
           lastModified:
             updated && created !== updated ? new Date(Number(updated)) : new Date(Number(created)),
-          images: images?.map((src: string) => new URL(`/api/image${src}`, site.baseurl)),
+          images: images?.map((src: string) =>
+            new URL(`/api/image${src}`, site.baseurl).toString(),
+          ),
+          alternates: alternates(url),
         };
       }),
-    ]);
+    ];
+
+    redis.json.set("sitemap", ".", items).then(() => {
+      console.info("cache stored..");
+      expired = Date.now() + HOUR;
+    });
+
+    return Response.json(items);
   }
+
   if (feedEnum.rss === type || feedEnum.rss2 === type)
     return new Response(feed.rss2(), { headers: { "Content-Type": "application/xml" } });
 
